0c63f57fedffc385935467e17e3fa218b5ac30aa
lil-shimon <shimon101018@gmail.com>
2026-02-28 01:44:44 +0900

    update

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Exchanges\MexcClient;
+
 class WalletController extends Controller
 {
-    //
+    public function __construct(
+        private MexcClient $mexcClient
+    ) {}
+
+    public function index()
+    {
+        $data = $this->mexcClient->getBalances();
+
+        return inertia('wallet/index', [
+            'balances' => $data,
+        ]);
+    }
 }
