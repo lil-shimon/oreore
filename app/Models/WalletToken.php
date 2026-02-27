fa0641d0695e4bb8a2f51fa77ccdd52e413ec812
lil-shimon <shimon101018@gmail.com>
2026-02-28 00:27:47 +0900

    update

--- app/Models/WalletToken.php
+++ app/Models/WalletToken.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Token;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WalletToken extends Model
 {
-    //
+    public function tokens(): BelongsTo
+    {
+        return $this->belongsTo(Token::class);
+    }
 }
