07fca5d00729d9c2770cbab2bef3d382dbda70e5
lil-shimon <shimon101018@gmail.com>
2026-02-28 00:25:22 +0900

    update

--- app/Models/Exchange.php
+++ app/Models/Exchange.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\WalletRecord;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Exchange extends Model
 {
-    //
+    public function walletRecords(): HasMany
+    {
+        return $this->hasMany(WalletRecord::class);
+    }
 }
