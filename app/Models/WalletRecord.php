3922eceb0afe3136a81aecc7f6d7140da1c6aa04
lil-shimon <shimon101018@gmail.com>
2026-02-28 00:26:52 +0900

    update

--- app/Models/WalletRecord.php
+++ app/Models/WalletRecord.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\WalletToken;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class WalletRecord extends Model
 {
-    //
+    public function walletTokens(): HasMany
+    {
+        return $this->hasMany(WalletToken::class);
+    }
 }
